b30a643278c4f1cf230b87f1bfd0490156fc63aa
Giacomo <giacomo@balzo.eu>
2020-12-16 15:34:00 +0100

    Fix compile sdk version

--- buildSrc/src/main/java/ProjectConfiguration.kt
+++ buildSrc/src/main/java/ProjectConfiguration.kt
@@ -2,7 +2,7 @@ object ProjectConfiguration {
 
     const val min_sdk = 23
     const val target_sdk = 30
-    const val compile_sdk = 39
+    const val compile_sdk = 30
     const val version_code = 1
     const val version_name = "0.0.1"
 
